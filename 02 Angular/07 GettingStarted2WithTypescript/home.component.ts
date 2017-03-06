af3b104e0fd1bdc5169497deab951d167b6d3971
ocalvo <ori.calvo@gmail.com>
2017-03-06 10:09:40 +0200

    More

---
+++ 02 Angular/07 GettingStarted2WithTypescript/home.component.ts
@@ -0,0 +1,29 @@
+class HomeController {
+    contacts;
+    name;
+
+    constructor() {
+        this.contacts = [
+            {id:1, name: "Ori"},
+            {id:2, name: "Roni"},
+        ];
+    }
+
+    add() {
+        if(!this.name) {
+            return;
+        }
+
+        const contact = {
+            id: -1,
+            name: this.name,
+        };
+
+        this.contacts.push(contact);
+    }
+}
+
+appModule.component("myHome", {
+    controller: HomeController,
+    templateUrl: "home.component.html",
+});
